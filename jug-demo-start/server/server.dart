98899c728ab6b07a2e2314d32700c30b9a26b793
Guillaume Girou <girou.g@sfeir.com>
2012-09-09 21:49:42 +0200

    Lighter jug demo

--- jug-demo-start/server/server.dart
+++ jug-demo-start/server/server.dart
@@ -22,8 +22,27 @@ class TickHandler {
   
   tick(var _timer) {
     counter++;
-    print("Counter: $counter");
-    // TODO send something
+    // TODO 5 Server side timer
+  }
+  
+  send(String value) {
+    if(timer != null) {
+      print("Send: $value");
+      connections.forEach((conn) => conn.send(value));
+    }
+  }
+  
+  Set<WebSocketConnection> connections = new Set<WebSocketConnection>();
+  onOpen(WebSocketConnection conn) {
+    conn.onMessage = (message) {
+      print('Message received: $message');
+      // TODO 6 : Stop server side timer on click
+    };
+
+    print('New WebSocket connection');
+    connections.add(conn);
+    conn.onClosed = (status, reason) => connections.remove(conn);
+    conn.onError = (e) => connections.remove(conn);
   }
 }
 
@@ -34,7 +53,9 @@ main() {
   HttpServer server = new HttpServer();
   server.onError = (error) => print(error);
   
-  // TODO set an handler
+  var handler = new WebSocketHandler();
+  handler.onOpen = new TickHandler().onOpen;
+  // TODO set the default handler
   
   server.listen('127.0.0.1', port);
   print('listening for connections on http://127.0.0.1:$port');
